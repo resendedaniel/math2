1f0b96fb4b3c3d3be23edec6c0ca200808092538
Daniel Resende <resende.daniel@gmail.com>
2016-04-15 10:03:42 -0300

    add transformation matrix

---
+++ 025-transformation_matrix/main.R
@@ -0,0 +1,19 @@
+library(MASS)
+library(ggplot2)
+
+P_matrix <- matrix(c(
+    1, 0,
+    1, 4,
+    -3, 4
+), 2)
+T_matrix <- matrix(c(
+    2, 1,
+    -1, 2
+), 2)
+
+T_points <- T_matrix %*% P_matrix
+
+df <- data.frame(t(cbind(P_matrix, T_points)), type=rep(c("original", "translated"), each=3))
+names(df)[1:2] <- c("x", "y")
+
+ggplot(df, aes(x, y, fill=type, alpha=0.1)) + geom_polygon()
